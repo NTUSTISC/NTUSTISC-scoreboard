ad0ed87d1e2754fa9b2d1d9bf51d763a0f52ad26
Jellyfish <turtle20953809@gmail.com>
2018-11-08 16:17:35 +0800

    add type

--- index/admin.py
+++ index/admin.py
@@ -1,6 +1,33 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Username)
-admin.site.register(Challenge)
-admin.site.register(Submit)
+class UsernameAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['user', 'name', 'skype', 'detail', 'picture']}),
+	]
+	inlines = [BookInline, CourseInline]
+	list_display = ('id', 'user', 'name')
+	list_filter = ['user', 'name']
+	search_fields = ['user', 'name']
+
+class ChallengeAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['user', 'name', 'skype', 'detail', 'picture']}),
+	]
+	inlines = [BookInline, CourseInline]
+	list_display = ('id', 'user', 'name')
+	list_filter = ['user', 'name']
+	search_fields = ['user', 'name']
+
+class SubmitAdmin(admin.ModelAdmin):
+	fieldsets = [
+		(None, {'fields': ['user', 'name', 'skype', 'detail', 'picture']}),
+	]
+	inlines = [BookInline, CourseInline]
+	list_display = ('id', 'user', 'name')
+	list_filter = ['user', 'name']
+	search_fields = ['user', 'name']
+
+admin.site.register(Username, UsernameAdmin)
+admin.site.register(Challenge, ChallengeAdmin)
+admin.site.register(Submit, SubmitAdmin)
