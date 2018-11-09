d88ca079622e4f612196cd044543ccca56a876d3
Jellyfish <turtle20953809@gmail.com>
2018-11-10 00:09:10 +0800

    change database

--- scoreboard/settings.py
+++ scoreboard/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'scoreboard.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'scoreboard',
+        'USER': 'root',
+        'PASSWORD': 'J3llyF1sh?',
+        'HOST': '0.0.0.0',
+        'PORT': '5432',
     }
 }
 
@@ -120,3 +124,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+
