f2388390a8ae396c941e0adc80f92d43af25c038
killua4564 <turtle20953809@gmail.com>
2019-06-04 13:14:23 +0800

    [What] - hotfix url.py

--- scoreboard/urls.py
+++ scoreboard/urls.py
@@ -5,10 +5,10 @@ from index import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', views.index),
-    url(r'^scoreboard$', views.scoreboard),
+    url(r'^scoreboard/$', views.scoreboard),
     url(r'^login/$', views.login),
     url(r'^logout/$', views.logout),
     url(r'^flag/$', views.flag),
     url(r'^ctf_finish/$', views.ctf_finish)
+    url(r'^$', views.index),
 ]
